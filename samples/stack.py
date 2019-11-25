86984229031d23cac785f134078505656d7f8b3c
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-11-25 10:37:56 +0300

    day 2 initial commit

--- samples/stack.py
+++ samples/stack.py
@@ -1,17 +1,22 @@
-class stack:
-    def __init__(self):
-        self.sourse = []
+class stackClass:
+    def __init__(self, size):
+        self.size = size
+        self.source = [0 for i in range(self.size)] 
         self.top = -1
 
-    def ceckEmpty(self):
-        return self.top
-    
+    def isEmpty(self):
+        return True if self.top == -1 else  False 
+
+    def isFull(self):
+        return True if self.top == self.size else  False 
+
     def pop(self):
+        if !isEmppty()
         self.top -= 1
         return top
 
     def push(self, value):
         self.top += 1
-        self.sourse[top] = value
+        self.source[self.top] = value
 
     
--- test/simpletest.py
+++ test/simpletest.py
@@ -1,6 +1,30 @@
 import sys
 sys.path.append('~/Documents/GitHub/AddisAlgo/')
 
-import samples
+from samples import stack
 
-print('success')
+class simpleTestClass:
+    def __init__(self):
+        pass
+
+    def testStack(self):
+        self.stack = stack.stackClass(7)
+        print(self.stack.isFull())
+        print(self.stack.source, ' ', self.stack.isEmpty())
+
+        self.stack.push(1)
+
+        self.stack.push(2)
+
+        self.stack.push(3)
+
+        self.stack.push(4)
+
+        print(self.stack.source, ' ', self.stack.isEmpty())
+
+
+
+
+if __name__ == '__main__':
+    obj = simpleTestClass()
+    obj.testStack()
