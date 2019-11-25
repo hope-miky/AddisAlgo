c0d2e21db7b3fdf9e89888baeae1d8c3d3427f76
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-11-25 15:02:24 +0300

    final touch to setup

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="AddisAlgo", # Replace with your own username
+    name="AddisAlgo",
     version="0.0.1",
     author="Tesfamichael Molla Ali",
     author_email="hope.miky1074@gmail.com",
-    description="A small example package",
+    description="Algorithm library for python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.0',
 )
