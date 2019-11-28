e0c1966e611d545dd02dc4b41493e4370251a728
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-11-28 22:51:27 +0300

    updated setup

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setuptools.setup(
     version="0.1",
     author="Tesfamichael Molla Ali",
     author_email="hope.miky1074@gmail.com",
-    description="Algorithm library for python",
+    description="A Python library consisting all the basic implementation of Algorithms as a submodule. The libarary also consists all the datastructures in python and some operations that can be done on them.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Tesfamichael1074/AddisAlgo",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
