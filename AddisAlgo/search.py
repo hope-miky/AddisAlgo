f0ff46cd58a99f0e348b84eee25f0f3ca2a898f4
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-12-02 20:42:43 +0300

    binary search

--- AddisAlgo/search.py
+++ AddisAlgo/search.py
@@ -1,15 +1,35 @@
 class search:
-    def __init__(self, value, source, type):
-        self.value = value
-        self.source = source
-        self.type = type
+    def __init__(self):
+        self.val = []
+        self.last = 0
+        self.first = 0
+        self.mid = 0
+        self.temp = 0
 
-        if self.type == "linear":
-            self.linearSearch()
 
-    def linearSearch(self):
-        for i in range(len(self.source)):
-            if self.source[i] == self.value:
-                return i
+    def Linear(self, value, source):
+        for i in range(len(source)):
+            if source[i] == value:
+                self.val.append(i)
         
-        return None 
+        return self.val
+
+        
+    def Binary(self, value, source):
+        self.last = len(source) - 1
+        self.mid = self.midFinder(self.first, self.last)
+        while value != source[self.mid]:
+            if value > source[self.mid]:
+                self.first = self.mid + 1
+                self.mid = self.midFinder(self.first, self.last)
+            elif value < source[self.mid]:
+                self.last = self.mid -1
+                self.mid = self.midFinder(self.first, self.last)
+            if self.first == self.mid:
+                print("Item not found")
+                return None
+
+        return self.mid
+    def midFinder(self, f, l):
+        return self.first + (l-f)/2
+         
--- test/searchtest.py
+++ test/searchtest.py
@@ -5,10 +5,11 @@ from AddisAlgo.search import search
 
 class simpleTestClass:
     def __init__(self):
+        self.search = search()
         pass
 
     def linearSearch(self):
-        print(search(value=1, source=[2,5,4,7,8,1,9,8,7,2,3,7], type="linear"))
+        print(self.search.Linear(value=1, source=[1,5,4,7,8,1,9,8,7,2,3,7], type="linear"))
     
 if __name__ == '__main__':
     obj = simpleTestClass()
