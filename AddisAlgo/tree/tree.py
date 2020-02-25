f150f2b24ba8632f52296ea94ce4fb24f122a508
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-02-25 10:24:55 +0300

    updated tree class

--- AddisAlgo/tree/tree.py
+++ AddisAlgo/tree/tree.py
@@ -1,8 +1,28 @@
 class Node:
     def __init__(self):
         self.data = ""
+        self.childs = []
 
 class Tree:
     def __init__(self):
-        self.childs = []
-        self.node = Node
+        self.temp = 0
+        #self.node = Node()
+        self.root = Node()
+    
+    def addChild(self, node):
+        node.childs.push(node)
+
+    def printTree(self, root):
+        print(root.data)
+        while root:
+            for i in root.childs:
+                print(i.data)
+                if len(i.childs) > 0:
+                    slef.printTree(i)
+
+
+if __name__ == '__main__':
+    
+        
+
+
